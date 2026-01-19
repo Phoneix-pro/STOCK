e890f6a369f99cc6366ce37349f8562efbc1767c
AKASH <ganeshsudha1224@gmail.com>
2026-01-19 10:57:37 +0530

    fix commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'
 import DashBoard from './page/DashBoard'
 import Production from './page/Production'
 import Stocks from './page/Stocks'
