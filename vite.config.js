54f15599abc6548deab54a60289f12046632b5f3
AKASH <ganeshsudha1224@gmail.com>
2026-01-19 11:21:07 +0530

    netlify commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-      base: "https://phoneix-pro.github.io/STOCK/",
     plugins: [react()],
 })
